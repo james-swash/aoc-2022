f2bfca292c40e65cb2b7c794d7f518083c1ed508
james swash <james.swash@tocabot.io>
2023-01-18 21:23:16 +0000

    day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -15,8 +15,36 @@ fn part_one(input: &str) -> usize {
         .sum()
 }
 
+fn part_two(input: &str) -> usize {
+    let lines: Vec<_> = input.lines().collect();
+    let chunks = lines.chunks(3);
+    let mut tmp: Vec<usize> = Vec::with_capacity(chunks.len());
+    for chunk in chunks {
+        for a in chunk[0].chars() {
+            if chunk[1].chars().any(|b| b == a) {
+                if let Some(c) = chunk[2].chars().find(|c| *c == a) {
+                    if c.is_uppercase() {
+                        tmp.push(c as usize - 38);
+                    } else {
+                        tmp.push(c as usize - 96);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    tmp.iter().sum()
+}
+
 fn main() {
     let input = include_str!("../../input_day3.txt");
+    let start_1 = Instant::now();
     let part_one = part_one(input);
-    println!("{part_one}");
+    let dur_1 = start_1.elapsed();
+    println!("{part_one}, {dur_1:?}");
+    use std::time::Instant;
+    let start = Instant::now();
+    let part_two = part_two(input);
+    let duration = start.elapsed();
+    println!("{part_two}, {duration:?}");
 }
